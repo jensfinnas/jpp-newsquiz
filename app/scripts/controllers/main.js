782adcbf01a592eec007a341ffd54973aed4c659
jensfinnas <jens.finnas@gmail.com>
2015-06-06 13:22:17 +0200

    Can answer questions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,47 @@
  */
 angular.module('newsquizApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var settings = {
+    	secondsPerQuestion: 5,
+    	numberOfLifes: 3 
+    }
+    var questions = [
+        {
+            question: 'Is 1+1=2?',
+            correctAnswer: 'yes',
+
+        },
+        {
+            question: 'Is 2+2=5?',
+            correctAnswer: 'no'
+
+        },
+        {
+            question: 'Is 2+6=8?',
+            correctAnswer: 'yes'
+
+        }
+    ]
+
+    $scope.lifesLeft = settings.numberOfLifes;
+
+    $scope.currentQuestionIndex = 0;
+    $scope.$watch('currentQuestionIndex', function(newVal) {
+    	$scope.currentQuestion = questions[$scope.currentQuestionIndex];
+    })
+    
+    
+
+    $scope.answer = function(answer) {
+    	// Correct answer
+    	if (answer == $scope.currentQuestion.correctAnswer) {
+
+    	}
+    	// Wrong answer
+    	else {
+    		$scope.lifesLeft = $scope.lifesLeft - 1;
+    	}
+    	$scope.currentQuestionIndex = $scope.currentQuestionIndex + 1;
+    }
+
   });
